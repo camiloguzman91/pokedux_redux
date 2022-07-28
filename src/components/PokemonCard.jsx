0bcd68ff9ea650bad3381835e30ce430cbe5a078
Camilo Guzman <camiloge_91@hotmail.com>
2022-07-28 13:50:38 -0500

    starting_project

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,20 @@
+import { StarOutlined } from '@ant-design/icons';
+import { Card } from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import './PokemonList.css';
+
+const PokemonCard = () => {
+    return (
+        <Card 
+            style={{ width: 250 }}
+            title='Ditto' 
+            cover={<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png' 
+            alt='Ditto' />}
+            extra={<StarOutlined />}
+        >
+            <Meta description='fire, magic' />
+        </Card>
+    );
+};
+
+export default PokemonCard;
